9a212d745442e8e0ca672036f168ff35e811f528
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-04-18 13:06:43 +0800

    Added MongoDB Db name and Collection Name

--- Desktop/backend.py
+++ Desktop/backend.py
@@ -2,12 +2,8 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import psutil
 
-
-
 app = Flask(__name__)
-
 CORS(app)
-
 @app.route('/processes', methods=['GET'])
 def get_process():
     processes = [p.info for p in psutil.process_iter(['pid', 'name'])]
