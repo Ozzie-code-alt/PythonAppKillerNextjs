c33939413d97d47a2d054108e3278f63877b2efa
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-04-18 14:59:01 +0800

    Worked on Login Page

--- Desktop/desktopDbRoutes.py
+++ Desktop/desktopDbRoutes.py
@@ -1,16 +1,16 @@
-import tkinter as tk
+
 from pymongo import MongoClient
+def insertData():
+    client = MongoClient('mongodb+srv://justin:justin123@cluster0.lhij5pu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
-client = MongoClient('mongodb+srv://justin:justin123@cluster0.lhij5pu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-
-db = client['thesisDb'] # database name this will be created if not present
-collection = db['thesisCollection'] # collection name
+    db = client['thesisDb'] # database name this will be created if not present
+    collection = db['thesisCollection'] # collection name
 
 
-document = {"name" : "Justin", "age" : 22}
-insertedDoc = collection.insert_one(document)
+    document = {"name" : "Justin", "age" : 22}
+    insertedDoc = collection.insert_one(document)
 
-print("Inserted Document Success")
-print("Document inserted with id: ", insertedDoc.inserted_id)
+    print("Inserted Document Success")
+    print("Document inserted with id: ", insertedDoc.inserted_id)
 
-client.close()
+    client.close()
