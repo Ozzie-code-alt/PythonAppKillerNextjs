ae11c83a401ec039dce9394592c29b6a33a278ff
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-05-05 21:08:54 +0800

    changes in admin / server

--- Server/admin/app/page.tsx
+++ Server/admin/app/page.tsx
@@ -1,36 +1,40 @@
-"use client"
-
 import { useEffect, useState } from "react";
-
-interface Process {
-  name: string;
-}
+import ShuffleHero from "@/components/ShuffleHero";
+import HoverDevSignup from "@/components/HoverDevSignup";
+// interface Process {
+//   name: string;
+// }
 
 export default function Home() {
-  const [grabProcess, setGrabProcess] = useState<Process[]>([]);
+  // const [grabProcess, setGrabProcess] = useState<Process[]>([]);
 
-  useEffect(() => {
-    try {
-      const GrabData = async () => {
-        await fetch("http://127.0.0.1:5000/processes")
-          .then((res) => res.json())
-          .then((data) => setGrabProcess(data));
-      };
-        GrabData();
-      
-    } catch (error) {
-      console.log("Fetch Data Error", error);
-    }
+  // useEffect(() => {
+  //   try {
+  //     const GrabData = async () => {
+  //       await fetch("http://127.0.0.1:5000/processes")
+  //         .then((res) => res.json())
+  //         .then((data) => setGrabProcess(data));
+  //     };
+  //       GrabData();
 
-  }, []);
+  //   } catch (error) {
+  //     console.log("Fetch Data Error", error);
+  //   }
+
+  // }, []);
   return (
-    <div className="h-svh w-dvh bg-black">
-      <p className="text-white">This is Current Data</p>
+    <div className="h-svh w-dvh">
+      {/* <p className="text-white">This is Current Data</p>
       <ul className="text-white">
         {grabProcess.map((processes,index) =>{
           return <li className="text-white" key={index}>{processes.name}</li>
         })}
-      </ul>
+      </ul> */}
+      <div className=" flex flex-col">
+        <ShuffleHero />
+        <HoverDevSignup />
+
+      </div>
     </div>
   );
 }
